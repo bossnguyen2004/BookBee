bc82c26f6b9507068a3df39455265777565933bd
DESKTOP-MMEUCNO\admin <trannguyen2004ns@gmail.com>
2024-11-01 02:30:49 +0700

    no message

--- BE/BookBee/obj/Debug/net8.0/BookBee.AssemblyInfo.cs
+++ BE/BookBee/obj/Debug/net8.0/BookBee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookBee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce1cfd22b6ca4bc41040d0a15c9ced2617a20b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookBee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookBee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
